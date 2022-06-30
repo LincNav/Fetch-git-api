66316147f4bd85b0df839973bccf695e8e2baa57
Lincoln Samuels <lincoln_samuels@hotmail.com>
2022-06-30 18:44:13 -0400

    adding events to my method

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // languages and progress  bar
 //lang repo rights on the same line
+const url = "https://api.github.com/users/tmtanzeel/repos";
+
+function generateSearch(event) {
+
+    event.preventDefault();
+    console.log('data');
+}
+
